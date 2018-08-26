5813235e441d2da274f928c30bb6042222845b8e
TechSupport420 <30539703+TechSupport420@users.noreply.github.com>
2018-08-26 23:59:43 +1000

    Seperate connection, List, Get, Delay
    Each command opens a new connection
    List, Get and delay are executed as intended

--- client.c
+++ client.c
@@ -5,36 +5,38 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <wordexp.h>
 
-#define PORT 42069
-#define BUFFER_SIZE 256
+#define PORT 46564
+#define COMMAND_BUFFER_SIZE 512
+#define MESSAGE_BUFFER_SIZE 1024
 
-void main(int argc, char** argv){
+typedef enum { false, true } bool;
 
-    // require server name
-    if(argc < 2){
-        printf("IP argument required\n");
-        return;
-    }
+void log(char* s){
+    if(true)
+        printf("Log: %s\n", s);
+}
 
+int connectToServer(char* server_ip){
+    
     // variables
-    int fd, code;
+    int fd;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-    char buffer[BUFFER_SIZE];
 
     // create socket
     fd = socket(AF_INET, SOCK_STREAM, 0);
     if(fd == -1){
         printf("Error opening socket\n");
-        return;
+        return 0;
     }
 
     // get host from args into server struct
-    server = gethostbyname(argv[1]);
+    server = gethostbyname(server_ip);
     if (server == NULL) {
         printf("Error no such host\n");
-        return;
+        return 0;
     }
 
     // setup serv_addr struct for socket
@@ -47,37 +49,284 @@ void main(int argc, char** argv){
 
     // connect to socket
     if (connect(fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
-        printf("Error connecting");
+        printf("Error connecting\n");
+        return 0;
+    }
+
+    return fd;
+}
+
+// write string to socket (send)
+int sendToServer(int socket_fd, char* message){
+
+    // message length
+    int length = strlen(message) + 1;
+
+    // get string representation of length of message
+    char message_size_str[MESSAGE_BUFFER_SIZE];
+    sprintf(message_size_str, "%i", length);
+
+    // send size of actual message
+    int code = write(socket_fd, message_size_str, MESSAGE_BUFFER_SIZE);
+    if(code < 0){
+        printf("Error writing to socket\n");
+        return code;
+    }
+
+    // send actual message
+    code = write(socket_fd, message, length);
+    if(code < 0){
+        printf("Error writing to socket\n");
+        return code;
+    }
+    
+    return code;
+}
+
+// wait for response and read it into buffer
+char* recieveFromServer(int socket_fd, int* len){
+
+    // read response size
+    char message_size_str[MESSAGE_BUFFER_SIZE];
+    bzero(message_size_str, MESSAGE_BUFFER_SIZE);
+    int code = read(socket_fd, message_size_str, MESSAGE_BUFFER_SIZE);
+    if (code < 0)
+        printf("Error reading from socket\n");
+
+    // create string to contain message
+    int length = atoi(message_size_str);
+    *len = length;
+    char* message = calloc(length + 1, sizeof(char));
+
+    // read actual response
+    code = read(socket_fd, message, length);
+    if (code < 0)
+        printf("Error reading from socket\n");
+
+    return message;
+}
+
+void print(char* message){
+    printf("%s\n", message);
+
+    // TODO print 40 lines at a time
+}
+
+void writeFile(char* filename, char* message, long length){
+    FILE *fp;
+    fp = fopen(filename, "w");
+    fwrite(message, sizeof(char), length, fp);
+
+    fclose(fp);
+}
+
+void processCommand(int argc, char** argv, char* server_ip){
+
+    if(argc < 1){
+        printf("No Command provided\n");
         return;
     }
 
-    // continuous cpmmand execution loop
+    int socket_fd = connectToServer(server_ip);
+    if(socket_fd == 0)
+        return;
+
+    char* command = argv[0];
+	if(strcmp(command, "list") == 0){ // list [-l] [-f] [pathname] [localfile]
+
+		// default argument values
+		bool l = false;
+		bool f = false;
+		char* pathname = "";
+		char* localfile = "";
+
+		// get arguments from command line into corresponding variables
+		int i = 1;
+		if(i < argc && strcmp(argv[i], "-l") == 0){
+			l = true;
+			i++;
+		}
+		if(i < argc && strcmp(argv[i], "-f") == 0){
+			f = true;
+			i++;
+		}
+		if(i < argc){
+			pathname = argv[i];
+			i++;
+		}
+		if(i < argc){
+			localfile = argv[i];
+			i++;
+		}
+
+		// compose the command and arguments
+		char sc[300] = "";
+		char* server_command_args = sc;
+		//strcat(shell_command, "ls");
+		if(l){
+			strcat(server_command_args, " -l");
+		}
+		if(pathname[0] != NULL){
+			strcat(server_command_args, " ");
+			strcat(server_command_args, pathname);
+		}
+
+        int code = sendToServer(socket_fd, "list");
+        if(code >= 0)
+            code = sendToServer(socket_fd, server_command_args);
+        if(code < 0){
+            printf("Error sending request\n");
+            close(socket_fd);
+            return;
+        }
+
+        int length;
+        char* response = recieveFromServer(socket_fd, &length);
+
+        if(localfile[0] == 0){ // if no local file specified (print to screen)
+            print(response);
+        }else{ // write response to file
+
+        }
+
+
+	}else if(strcmp(command, "get") == 0){ // get filepath [-f] [localfile]
+
+		// default argument values
+		bool f = false;
+		char* filepath = "";
+		char* localfile = "";
+
+		// get arguments from command line into corresponding variables
+		if(argc <= 1){
+			printf("No filepath provided\n");
+			return;
+		}
+		filepath = argv[1];
+
+		int i = 2;
+		if(i < argc && strcmp(argv[i], "-f") == 0){
+			f = true;
+			i++;
+		}
+		if(i < argc){
+			localfile = argv[i];
+			i++;
+		}
+
+        int code = sendToServer(socket_fd, "get");
+        if(code >= 0)
+            code = sendToServer(socket_fd, filepath);
+        if(code < 0){
+            printf("Error sending request\n");
+            close(socket_fd);
+            return;
+        }
+
+        int length;
+        char* response = recieveFromServer(socket_fd, &length);
+
+        if(localfile[0] == 0){ // if no local file specified (print to screen)
+            // print(response);
+            printf("%.*s\n", length, response);
+        }else{ // write response to file
+            writeFile(localfile, response, length);
+        }
+
+	}else if(strcmp(command, "put") == 0){ // put localfile [-f] [newname]: copy file to server
+
+	}else if(strcmp(command, "sys") == 0){ // sys
+
+		// #ifdef _WIN32
+		// SYSTEM_INFO siSysInfo;
+		// GetSystemInfo(&siSysInfo);
+
+		// printf("Hardware information: \n");  
+		// printf("  OEM ID: %u\n", siSysInfo.dwOemId);
+		// printf("  Number of processors: %u\n", 
+		// 	siSysInfo.dwNumberOfProcessors); 
+		// printf("  Page size: %u\n", siSysInfo.dwPageSize); 
+		// printf("  Processor type: %u\n", siSysInfo.dwProcessorType); 
+		// printf("  Minimum application address: %lx\n", 
+		// 	siSysInfo.lpMinimumApplicationAddress); 
+		// printf("  Maximum application address: %lx\n", 
+		// 	siSysInfo.lpMaximumApplicationAddress); 
+		// printf("  Active processor mask: %u\n", 
+		// 	siSysInfo.dwActiveProcessorMask);
+		// #endif
+
+	}else if(strcmp(command, "delay") == 0){ // delay integer
+		int delay = 0;
+
+		// get arguments from command line into corresponding variables
+		if(argc <= 1){
+			printf("No delay provided\n");
+            close(socket_fd);
+			return;
+		}
+		
+        // send command and delay to server
+        int code = sendToServer(socket_fd, "delay");
+        if(code >= 0)
+            code = sendToServer(socket_fd, argv[1]);
+        if(code < 0){
+            printf("Error sending request\n");
+            close(socket_fd);
+            return;
+        }
+
+        // get and display respons
+        int length;
+        char* response = recieveFromServer(socket_fd, &length);
+        print(response);
+        
+	}else{ // unknown command
+        printf("Command not recognised\n");
+    }
+
+    close(socket_fd);
+    // TODO close socket
+}
+
+void main(int argc, char** argv){
+
+    // require server name
+    if(argc < 2){
+        printf("IP argument required\n");
+        return;
+    }
+    char* server_ip = argv[1];
+
+    char cmd_buffer[COMMAND_BUFFER_SIZE];
+    // char* cmd_buffer = malloc(COMMAND_BUFFER_SIZE);
+
+    // continuous command execution loop
     while(1){
         // query for command
         printf("> ");
-        bzero(buffer, BUFFER_SIZE);
-        fgets(buffer, 255, stdin);
+        bzero(cmd_buffer, COMMAND_BUFFER_SIZE);
+        fgets(cmd_buffer, COMMAND_BUFFER_SIZE - 1, stdin);
+        wordexp_t in_arg_struct;
+        char **in_argv;
+        int in_argc = 0;
+
+        // remove new line character at the end if input
+        cmd_buffer[strlen(cmd_buffer) - 1] = 0;
 
         // if command is exit, break loop before sending any command
-        if(strcmp(buffer, "exit\n") == 0)
+        if(strcmp(cmd_buffer, "exit") == 0)
             break;
 
-        // write entered string to socket (send)
-        code = write(fd, buffer, strlen(buffer));
-        if (code < 0){
-            printf("Error writing to socket\n");
-            continue;
-        }
-            
-        // wait for response and read it into buffer
-        bzero(buffer, BUFFER_SIZE);
-        code = read(fd, buffer, 255);
-        if (code < 0){
-            printf("Error reading from socket\n");
-            continue;
-        }
+        // break arguments individual strings
+        wordexp(cmd_buffer, &in_arg_struct, 0);
+        in_argv = in_arg_struct.we_wordv;
+        in_argc = in_arg_struct.we_wordc;
         
-        // print server response
-        printf("%s\n",buffer);
+        // for(int i = 0; i < in_argc; i++)
+        //     printf("%s\n", in_argv[i]);
+        // wordfree(&cmd_arguments_struct);
+
+        // process command
+        processCommand(in_argc, in_argv, server_ip);
     }
 }
--- server.c
+++ server.c
@@ -1,18 +1,70 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 
-#define PORT 42069
-#define BUFFER_SIZE 256
+#define PORT 46564
+#define MESSAGE_BUFFER_SIZE 1024
+
+// wait for request/command and then read it
+char* recieveFromClient(int socket_fd){
+
+    // read response size
+    char message_size_str[MESSAGE_BUFFER_SIZE];
+    bzero(message_size_str, MESSAGE_BUFFER_SIZE);
+    int code = read(socket_fd, message_size_str, MESSAGE_BUFFER_SIZE);
+    if (code < 0)
+        printf("Error reading from socket\n");
+
+    // create string to contain message
+    int size = atoi(message_size_str);
+    char* message = calloc(size + 1, sizeof(char));
+
+    // read actual response
+    code = read(socket_fd, message, size);
+    if (code < 0)
+        printf("Error reading from socket\n");
+
+    return message;
+}
+
+// send message to client
+void sendStringToClient(int socket_fd, char* message){
+    // message length
+    int length = strlen(message) + 1;
+    // send message
+    sendToClient(socket_fd, message, length);
+}
+
+// send message to client
+void sendToClient(int socket_fd, char* message, long length){
+
+    // get string representation of length of message
+    char message_size_str[MESSAGE_BUFFER_SIZE];
+    sprintf(message_size_str, "%i", length);
+
+    // send size of actual message
+    int code = write(socket_fd, message_size_str, MESSAGE_BUFFER_SIZE);
+    if(code < 0){
+        printf("Error writing to socket\n");
+        return;
+    }
+
+    // send actual message
+    code = write(socket_fd, message, length);
+    if(code < 0){
+        printf("Error writing to socket\n");
+        return;
+    }
+}
 
 void main(){
     // variables
     struct sockaddr_in addr;
     struct sockaddr client;
     int socket_fd, connection_socket_fd, code;
-    char buffer[BUFFER_SIZE];
 
     // create socket
     socket_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -39,36 +91,108 @@ void main(){
     // wait for connection
     listen(socket_fd, 5);
 
-    // accept any connection
-    int clienlen = sizeof(client);
-    connection_socket_fd = accept(socket_fd, &client, &clienlen);
-    if(connection_socket_fd < 0){
-        printf("Error on accept\n");
-        return;
-    }
-
-    // continuous loop of recieveing and executing commands
     while(1){
-        
-        // wait for request/command and then read it
-        bzero(buffer, BUFFER_SIZE);
-        code = read(connection_socket_fd, buffer, 255);
-        if(code < 0){
-            printf("Error reading from socket\n");
+        printf("Ready for connection\n");
+
+        // accept any connection
+        int clienlen = sizeof(client);
+        connection_socket_fd = accept(socket_fd, &client, &clienlen);
+        if(connection_socket_fd < 0){
+            printf("Error on accept\n");
             continue;
         }
+        
+        printf("New connection\n");
+        
+        // base command
+        char* command = recieveFromClient(connection_socket_fd);
 
         // print command and then execute it
-        printf("> %s", buffer);
-        int status = system(buffer);
+        printf("> %s\n", command);
 
-        // return status quote to client
-        char response[50];
-        sprintf(response, "Status code: %i", status);
-        code = write(connection_socket_fd, response, strlen(response));
-        if(code < 0){
-            printf("Error writing to socket\n");
-            continue;
+        if(strcmp(command, "list") == 0){ // list args
+
+            // get shell arguments
+            char* shell_args = recieveFromClient(connection_socket_fd);
+            char shell_base_command[] = "ls";
+            char* shell_command = calloc(strlen(shell_base_command) + strlen(shell_args) + 2, sizeof(char));
+            strcat(shell_command, shell_base_command);
+            strcat(shell_command, shell_args);
+
+            // execute command and store result, status and/or error
+            FILE *command_fp;
+            int status;
+
+            // execute
+            command_fp = popen(shell_command, "r");
+            if(command_fp == NULL){
+                printf("Unknown error\n");
+            }
+
+            // get length of file
+            int tmp_size = 128;
+            int str_max_size = 2*tmp_size;
+            char* tmp = calloc(tmp_size, sizeof(char));
+            char* response = calloc(str_max_size, sizeof(char));
+
+            // read file into string
+            while(fgets(tmp, tmp_size, command_fp) != NULL){
+
+                // if buffer is full, double size
+                if(strlen(response) + tmp_size + 1 >= str_max_size){
+                    str_max_size *= 2;
+                    char* new = calloc(str_max_size, sizeof(char));
+                    strcpy(new, response);
+                    free(response);
+                    response = new;
+                }
+
+                // append next section
+                strcat(response, tmp);
+            }
+            
+            // respond
+            sendStringToClient(connection_socket_fd, response);
+
+        }else if(strcmp(command, "get") == 0){ // get filepath
+        
+            // get filepath
+            char* filepath = recieveFromClient(connection_socket_fd);
+
+            char* buffer = 0;
+            long length;
+            FILE* f = fopen (filepath, "r");
+
+            if(f){
+                fseek (f, 0, SEEK_END);
+                length = ftell (f);
+                fseek (f, 0, SEEK_SET);
+
+                buffer = malloc(length);
+                if (buffer){
+                    fread(buffer, sizeof(char), length, f);
+                }
+                fclose (f);
+            }
+
+            sendToClient(connection_socket_fd, buffer, length);
+
+        }else if(strcmp(command, "put") == 0){
+            
+        }else if(strcmp(command, "sys") == 0){
+            
+        }else if(strcmp(command, "delay") == 0){ // delay integer
+            // read delay duration
+            char* delay_str = recieveFromClient(connection_socket_fd);
+            int delay = atoi(delay_str);
+
+            // sleep for specified time
+            sleep(delay);
+
+            // respond
+            sendStringToClient(connection_socket_fd, delay_str);
         }
+
+        close(connection_socket_fd);
     }
 }
